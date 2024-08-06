ad9e0d1449b7ac9c0d420e2cf59efa509dfdd2d6
MrinankGaur <mrinank2484@gmail.com>
2024-08-06 10:37:59 +0530

    changes

---
+++ Basics/Inputs.java
@@ -0,0 +1,10 @@
+package Basics;
+import java.util.*;
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter integer: ");
+        int x = sc.nextInt();
+        System.out.println("Your no is : " + x);
+    }
+}
---
+++ Basics/Primitives.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int x = 49;
+        char y = 'r';
+        float z = 97.44f;
+        double f = 343334.6453456;
+        long ed = 32354236467L;
+        boolean s = true;
+    }
+}
