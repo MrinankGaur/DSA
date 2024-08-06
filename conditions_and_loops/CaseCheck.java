502997972518056030d46f2c3e497fb503dfead0
MrinankGaur <mrinank2484@gmail.com>
2024-08-06 10:32:53 +0530

    added CaseCheck.java

--- conditions_and_loops/CaseCheck.java
+++ conditions_and_loops/CaseCheck.java
@@ -1,4 +1,17 @@
 package conditions_and_loops;
-
+import java.util.*;
 public class CaseCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        char x = sc.next().trim().charAt(0);
+        if (x>='a' && x<='z'){
+            System.out.println("input is in lowercase");
+        }else if(x>='A' && x<='Z'){
+            System.out.println("input is in uppercase");
+        }
+
+
+
+    }
 }
