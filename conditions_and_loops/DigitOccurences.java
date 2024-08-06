01f24834097ca4186d52372bce96a0aa6ee1d197
MrinankGaur <mrinank2484@gmail.com>
2024-08-06 11:26:31 +0530

    checking no of occurrences of a digit in a number

---
+++ conditions_and_loops/DigitOccurences.java
@@ -0,0 +1,19 @@
+package conditions_and_loops;
+import java.util.Scanner;
+public class DigitOccurences {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int n=999;
+        int count = 0;
+        while(n>0){
+            n = x%10;
+            x = x/10;
+            if(n==y){count++;}
+
+
+        }
+        System.out.println(count);
+    }
+}
