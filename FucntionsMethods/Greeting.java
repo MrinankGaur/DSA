aca14607c8b03b6a551f50b326e8d1ff221d8450
MrinankGaur <mrinank2484@gmail.com>
2024-08-07 18:18:28 +0530

    Method to print hello world

---
+++ FucntionsMethods/Greeting.java
@@ -0,0 +1,9 @@
+package FucntionsMethods;
+public class Greeting {
+    public static void main(String[] args) {
+       greeting();
+    }
+    static void greeting(){
+        System.out.println("Hello World");
+    }
+}
