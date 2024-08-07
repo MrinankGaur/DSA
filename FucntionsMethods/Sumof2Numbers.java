de1ebf526b6ca6336f99a9d26d76df8073380f80
MrinankGaur <mrinank2484@gmail.com>
2024-08-07 18:16:47 +0530

    Sum of 2 numbers using a method

---
+++ FucntionsMethods/Sumof2Numbers.java
@@ -0,0 +1,17 @@
+package FucntionsMethods;
+import java.util.*;
+public class Sumof2Numbers {
+    public static void main(String[] args) {
+      sum();
+    }
+    static void sum(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter no 1: ");
+        int x = sc.nextInt();
+        System.out.print("Enter no 2: ");
+        int y = sc.nextInt();
+        System.out.println("Sum of numbers is: "+(x+y));
+
+    }
+}
+
