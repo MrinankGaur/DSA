29ebfdb004216907500a30951a66a20b9ccecabe
MrinankGaur <mrinank2484@gmail.com>
2024-08-07 18:24:12 +0530

    Method with int parameters to print no.s

---
+++ FucntionsMethods/Sum2.java
@@ -0,0 +1,21 @@
+package FucntionsMethods;
+import java.util.*;
+public class Sum2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter no 1: ");
+        int m = sc.nextInt();
+        System.out.print("Enter no 2: ");
+        int n = sc.nextInt();
+
+        int x = sum(m,n);
+
+        System.out.println("Sum : "+x);
+    }
+    static int sum(int a,int b){
+
+        return a + b;
+
+    }
+}
+
