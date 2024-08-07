70202ff37f8f33d60b5274ad68c6b69d94e78082
MrinankGaur <mrinank2484@gmail.com>
2024-08-07 19:03:08 +0530

    method overloading example

---
+++ FucntionsMethods/MethodOverloading.java
@@ -0,0 +1,14 @@
+package FucntionsMethods;
+
+public class MethodOverloading {
+    public static void main(String[] args) {
+        fun(10);
+        fun("Sussysbaka");
+    }
+    static void fun(int a){
+        System.out.println(a);
+    }
+    static void fun(String s){
+        System.out.println(s);
+    }
+}
